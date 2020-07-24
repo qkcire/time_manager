3f38cbb9eb8775ed469aeaafae34ef8b719261a0
Erick Quintanilla <equintanilla@berkeley.edu>
2020-07-24 06:51:10 -0700

    Add countdown animation and exit procedure

--- time_manager.py
+++ time_manager.py
@@ -3,21 +3,34 @@
 import time
 from time import strftime
 import datetime as dt
+import os
 
 # displays the amount of time remaining on screen
 # according to the amount of time set in
 # MINUTES, 1 <= min <= 60
-def countdown(min):
+def countdown_display(min):
     start_time = dt.datetime.now()
     target_time = start_time + dt.timedelta(minutes = min)
     time_remaining = target_time - start_time
-    print("Begin countdown.")
+    # print("Begin countdown.")
+    # print("\t\tH:MM:SS")
+    os.system("clear")
+    print("\t\t\t\t     Time remaining")
     while str(time_remaining)[0:7] != '0:00:00':
         current_time = dt.datetime.now()
         time_remaining = target_time - current_time
-        print(str(time_remaining)[0:7], end="\r", flush=True)
+        print("\t\t\t\t\t"+str(time_remaining)[0:7], end="\r", flush=True)
         time.sleep(1)
     print(flush=True)
-    print("Time complete.")
+    # os.system("clear")
+    print("\t\t\t\t     Time complete")
+    input("\t\t\t\t  Press ENTER to exit")
+    os.system("clear")
 
-countdown(1)
+countdown_display(1)
+
+# def main():
+
+
+# if __name__ == "__main__":
+
