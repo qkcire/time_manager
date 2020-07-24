bb1522eeda4b3874181fe177c07c9637ed0823f5
Erick Quintanilla <equintanilla@berkeley.edu>
2020-07-24 05:02:36 -0700

    Create countdown method. Displays the amount of time remaining on screen according to the amount of input time in MINUTES

--- time_manager.py
+++ time_manager.py
@@ -1,2 +1,23 @@
 #!/home/eq/miniconda3/envs/time_manager/bin/python3.8
-print("Hello, world!")
+
+import time
+from time import strftime
+import datetime as dt
+
+# displays the amount of time remaining on screen
+# according to the amount of time set in
+# MINUTES, 1 <= min <= 60
+def countdown(min):
+    start_time = dt.datetime.now()
+    target_time = start_time + dt.timedelta(minutes = min)
+    time_remaining = target_time - start_time
+    print("Begin countdown.")
+    while str(time_remaining)[0:7] != '0:00:00':
+        current_time = dt.datetime.now()
+        time_remaining = target_time - current_time
+        print(str(time_remaining)[0:7], end="\r", flush=True)
+        time.sleep(1)
+    print(flush=True)
+    print("Time complete.")
+
+countdown(1)
