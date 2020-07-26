7eae581540d4d3ee77396b4f266120cd814ddc27
Erick Quintanilla <equintanilla@berkeley.edu>
2020-07-25 22:57:19 -0700

    Remove deprecriated method

--- time_manager.py
+++ time_manager.py
@@ -27,41 +27,6 @@ def menu():
         print("You selected " + str(select) + " as your work_num.")
     return select
 
-# output countdown timer according to MIN input
-# where: 1 <= MIN <= infinity
-def countdown_display(min, work_type):
-    start_time = dt.datetime.now()
-    target_time = start_time + dt.timedelta(minutes = min)
-    time_remaining = target_time - start_time
-    os.system("clear")
-    print(start_time)
-    print("Working on: " + work[work_type])
-    # print("TIME LEFT")
-    try:
-        while str(time_remaining)[0:7] != '0:00:00':
-            current_time = dt.datetime.now()
-            time_remaining = target_time - current_time
-            print(" TIME LEFT  "+str(time_remaining)[0:7], end="\r", flush=True)
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Keyboard pause initiated.")
-        print("Ending clock.")
-        return
-
-    end_time = dt.datetime.now()
-    print(flush=True)
-    print("TIME COMPLETE")
-    start_page, end_page = map(int, input("Enter the start page, end page: ").split(", "))
-    input("PRESS enter to SAVE DATA AND CONTINUE")
-    # start timestamp
-    start_time_tuple = int(start_time.strftime("%Y%m%d")), int(start_time.strftime("%H%M%S"))
-    start = int(str(start_time_tuple[0]) + str(start_time_tuple[1]))
-    # end timestamp
-    end_time_tuple = int(end_time.strftime("%Y%m%d")), int(end_time.strftime("%H%M%S"))
-    end = int(str(end_time_tuple[0]) + str(end_time_tuple[1]))
-    save_data(work_type, start, end, end_page, start_page)
-    os.system("clear")
-
 def alarm_clock():
     pass
 
@@ -70,7 +35,6 @@ def save_data(work_type, start_time, end_time):
     with open('data_time_manager.csv', 'a') as file:
         writer = csv.writer(file)
         writer.writerow([start, end, work_type])
-        # writer.writerow([work_type, start_time, end_time])
 
 def break_time():
     print("BREAK TIME!")
@@ -135,10 +99,7 @@ def main():
             print("You've chose to work on: " + work[work_num].upper())
             input("Press ENTER to start time")
             timer(work_num)
-            # countdown_display(5, work_num)
-            # break_time()
 
 
 if __name__ == "__main__":
-    # countdown_display(int(sys.argv[1]))
     main()
